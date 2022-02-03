2c8b674fb42ecca632e54a45bc66ce860ec29818
rjodriscoll <48929734+rjodriscoll@users.noreply.github.com>
2022-02-03 23:08:50 +0000

    push fixed dev.py

--- dev.py
+++ dev.py
@@ -1,91 +1,111 @@
 from english_words import english_words_set
 import random
-import string
 
 WORD_LENGTH = 5
-N_WORDS = 3
+N_GUESSES = 6
+N_WORDS = 1
 ENGLISH_WORDS_LIST = [*english_words_set]
-
-
-answers = AnswerGenerator()
-
+LIMIT = 10
 
 
 class GridGenerator:
-    def __init__(self, word_length = WORD_LENGTH, n_words = N_WORDS) -> None:
-        self._word_length = word_length
-        self._n_words =n_words 
+    def __init__(self, word_length = WORD_LENGTH, n_guesses = N_GUESSES) -> None:
+        self.word_length = word_length
+        self.n_guesses = n_guesses 
         self.empty_grid = self._generate_empty_grid()
 
     def _generate_empty_grid(self) -> list:
         row = []
         grid = []
-        for _ in range(self._word_length):
-            
+        for _ in range(self.word_length):
             row.append('?')
-        for _ in range(self._n_words):
+        for _ in range(self.n_guesses):
             grid.append(row)
         return grid 
 
 class AnswerGenerator:
     def __init__(self) -> None:
         self.eligible_words = [w.lower() for w in ENGLISH_WORDS_LIST if len(w) == WORD_LENGTH]
-        self.words = self._select_words()
+        self.word = self._select_word()
 
-    def _select_words(self) -> list:
-        return random.sample(self.eligible_words, N_WORDS)
+    def _select_word(self) -> list:
+        return random.sample(self.eligible_words, N_WORDS)[0]
 
 class WordGuesser:
     def __init__(self) -> None:
         self.eligible_words = [w.lower() for w in ENGLISH_WORDS_LIST if len(w) == WORD_LENGTH]
         self.remaining_words = self.eligible_words
         self.current_grid = GridGenerator().empty_grid
-        self.alphabet = list(string.ascii_lowercase)
-        self.temp_alphabet = self.alphabet
-        self.complete_words = [False] * N_WORDS
-        self.current_word = 0
+        self.word_guess = ['.']*WORD_LENGTH
+        self.letters_in_diff_position = []
+        self.letters_not_in_word = []
+        self.guesses = 0
+        self.current_row = 0
+        self.current_word = None
 
-    def get_remaining_indices(self):
-        return [i for i in range(len(self.current_grid[self.current_word])) if self.current_grid[self.current_word][i] == '?']
+    def guess_word(self):
+        if self.guesses == 0:
+            print(f"on guess {self.guesses} the word is about")
+            return 'about'
+        else:
+            word = random.sample(self.remaining_words, 1)[0]
+            print(f"on guess {self.guesses} the word is {word}")
+            return word
+       
 
-    def update_word_of_interest(self):
-        for i, b in enumerate(self.complete_words):
-            if b == False:
-                self.current_word = i 
-                break       
-
-    def reset_temp_alphabet(self):
-        self.temp_alphabet = self.alphabet
+    def insert_word_into_grid(self):
+        self.current_word = self.guess_word()
+        self.current_grid[self.current_row] = [c for c in self.current_word]
+        self.update_row()
     
-    def drop_letter_from_temp_alphabet(self, guess):
-        self.temp_alphabet.remove(guess)
-
-    def guess_letter(self):
-        return random.sample(self.temp_alphabet)
-
-    def evaluate_letter(self):
-        guess = self.guess_letter()
-        locs = self.get_remaining_indices()
-        loc = random.sample(locs, 1)
-
-        if answers[self.current_word][loc] == guess:
-            self.current_grid[self.current_word] = guess
-        elif guess in answers[current_word]:
-            locs.remove(loc)
-            while not locs:
-                loc = random.sample(locs, 1)
-                if answers[self.current_word][loc] == guess:
-                    self.current_grid[self.current_word] = guess
-                else: 
-                    locs.remove(loc)
-        elif guess not in answers[current_word]:
-           self.drop_letter_from_temp_alphabet()
-
+    def update_guesses(self):
+        self.guesses += 1
     
-    def guess_row(self):
-        while ''.join(self.current_grid[self.current_word]) != answers[current_word]:
-            self.evaluate_letter()
-            
-        if ''.join(self.current_grid[self.current_word]) == answers[current_word]:
+    def update_row(self):
+        self.current_row +=1
+    
+    def evaluate_word(self):
+        for index, letter in enumerate(self.current_word):
+            if answer[index] == letter:
+                self.word_guess[index] == letter
+            elif letter in answer:
+                self.letters_in_diff_position.append(letter)
+            elif letter not in answer:
+                self.letters_not_in_word.append(letter)
+
+    def reduce_potential_words(self):
+        for index, letter in enumerate(self.word_guess):
+            if letter == '.':
+                pass 
+            else:
+                self.remaining_words = [word for word in self.remaining_words if word[index] == letter]
+        
+        for letter in self.letters_in_diff_position:
+            self.remaining_words = [word for word in self.remaining_words if letter in word]
+
+        for letter in self.letters_not_in_word:
+            self.remaining_words = [word for word in self.remaining_words if letter not in word]
+
+        print(f"the number of remaining words is {len(self.remaining_words)}")
+
+
+    def play_game(self):
+        while self.guesses < LIMIT:
+            self.insert_word_into_grid()
+            self.evaluate_word()
+            self.reduce_potential_words()
+            self.update_guesses()
+
+            if len(self.remaining_words) == 1 and self.remaining_words[0] == answer:
+                print(f"solved with word {self.remaining_words[0]} in {self.guesses} guesses")
+                break
+            else: 
+                print(f"this program failed. You had {len(self.remaining_words)} potential words remaining")
+
+answers = AnswerGenerator()
+answer = answers.word
+
+w = WordGuesser()
+w.play_game()
 
 
